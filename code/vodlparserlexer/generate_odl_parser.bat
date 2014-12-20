b2f26f341143ae6fe30d1a21577ed925be3f8153
Paul Chabas <chabas.paul@gmail.com>
2014-12-20 16:30:19 +0100

    - comment.

--- code/vodlparserlexer/generate_odl_parser.bat
+++ code/vodlparserlexer/generate_odl_parser.bat
@@ -1,3 +1,5 @@
 ..\\..\\extern\\flexppbisonpp\bisonpp.exe -d -oOdlGenParser.cpp -hOdlGenParser.h OdlParser.y
+
+rem "copy of OdlParser.y in vcprojet directory to make file/line debug working :'("
 copy OdlParser.y ..\\..\\VS2013\\vodlparserlexer\\OdlParser.y
 pause
